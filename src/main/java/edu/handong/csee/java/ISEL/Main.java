ed0917c2513373d5cd94a01bc34508fa35a00dc7
sungbin <21700613@handong.edu>
2018-06-28 17:10:14 +0900

    I complete git-blame that it blame all .java files when I put git-directory.

--- src/main/java/edu/handong/csee/java/ISEL/Main.java
+++ src/main/java/edu/handong/csee/java/ISEL/Main.java
@@ -1,109 +1,45 @@
 package edu.handong.csee.java.ISEL;
 
 import java.io.File;
-import java.io.FileInputStream;
-
-import org.eclipse.jgit.api.BlameCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.blame.BlameResult;
 import org.eclipse.jgit.diff.RawText;
-import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.lib.RepositoryBuilder;
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
 public class Main {
 
 	public static void main(String args[]) {
 		try {
-
 			File directory = new File("/Users/imseongbin/documents/Java/BugPatchCollector");
-			//File directory = new File("/Users/imseongbin/documents/Java/BugPatchCollector/src/main/java/edu/handong/csee/java/ISEL");
+			
+			
 			Git git = Git.open(directory);
-			//Git git = Git.init().setDirectory(directory).call();
-			// Git.init().setDirectory("/Users/imseongbin/documents/Java/ThreadExamples").call();
-
-			//git.b;
-			
 			Repository repository = git.getRepository();
-			
-			File ndirectory = new File("/Users/imseongbin/documents/Java/BugPatchCollector/src/main/java/edu/handong/csee/java/ISEL"); //수정중..
-			
-			
-			
-			final String[] list = ndirectory.list();
-			
-			if (list == null) {
-				throw new IllegalStateException("Did not find any files at " + new File(".").getAbsolutePath());
-			} 
-			for (String file : list) {
-				String nfile = "src/main/java/edu/handong/csee/java/ISEL" + "/"+ file;
-				
-				if (new File(file).isDirectory()) {
-					continue;
-				}
-				System.out.println("Blaming " + nfile);
-				BlameResult result = new Git(repository).blame().setFilePath(nfile).call();
-				RawText rawText = result.getResultContents();
-				for (int i = 0; i < rawText.size(); i++) {
-					final PersonIdent sourceAuthor = result.getSourceAuthor(i);
-					final RevCommit sourceCommit = result.getSourceCommit(i);
-					System.out.println(sourceAuthor.getName()
-							+ (sourceCommit != null ? "/" + sourceCommit.getCommitTime() + "/" + sourceCommit.getName()
-									: "")
-							+ ": " + rawText.getString(i));
-
-				}
-			}/**/
-			String[] gitignoreList = null; //여기 수정 예정.
-			//recur(directory.toString(), repository, gitignoreList);
+			recur(directory.toString(), repository);
 			
 		} catch (Exception e) {
 			e.fillInStackTrace();
 		}
 	}
 	
-	public static void recur(String directory, Repository repository, String[] gitignoreList) throws GitAPIException {
+	public static void recur(String directory, Repository repository) throws GitAPIException {
 		
-		/*
-		System.out.println("우선여기 @@@@@" + directory);
-		for (String file : list) {
-			System.out.println("**"+file);
-		}/**/
-		
-
 		String[] list = new File(directory).list();
-		if (list == null) {
-			throw new IllegalStateException("Did not find any files at " + new File(directory).getAbsolutePath());
-		}
-		
-		
-		
-		
-		/* 여기 for문 안에서 file이 .gitignore파일에 존재하는 파일인지 체크하고 그렇다면 continue해야함
-		 * .gitignore에서는 3가지 패턴
-		 * (1) *.class	-> first star
-		 * (2) example*	-> last star
-		 * (3) file.tmp	-> exact file name
-		 */
 		for (String file : list) {
 			
 			String nfile = directory + "/" +file.toString();
 			
-			//System.out.println("File: " + nfile);
-			
 			if (new File(nfile).isDirectory()) {
-				//System.out.println("call recur");
-				recur(nfile,repository,gitignoreList);
+				recur(nfile,repository);
 			}
-			
-			
-			else  if(nfile.contains(".java") && !nfile.contains("Test")){ //여기도 수정 필요.
-				System.out.println("Blaming " + nfile);
-				final BlameResult result = new Git(repository).blame().setFilePath(nfile).call();
+			else if(file.contains(".java")){
+				String nnfile = nfile.substring(repository.getWorkTree().toString().length()+1);
+				
+				System.out.println("Blaming " + nnfile);
+				final BlameResult result = new Git(repository).blame().setFilePath(nnfile).call();
 				final RawText rawText = result.getResultContents();
 				for (int i = 0; i < rawText.size(); i++) {
 					final PersonIdent sourceAuthor = result.getSourceAuthor(i);
@@ -115,6 +51,6 @@ public class Main {
 				}
 			}
 		}
-		/**/
+
 	}
 }
